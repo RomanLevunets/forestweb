d33a00cc4bc7bf85a06fcca13fe6ec8a49d962c9
Roman <rromaromik@gmail.com>
2017-09-03 13:33:46 +0300

    rebuild

--- js/common.js
+++ js/common.js
@@ -48,5 +48,50 @@ $(document).ready(function() {
         });
     });
     showTopService();
+
+
+
+    //animation content
+    (function($) {
+        $.fn.animated = function(inEffect) {
+            $(this).each(function() {
+                var ths = $(this);
+                ths.css("opacity", "0").addClass("animated").waypoint(function(dir) {
+                    if (dir === "down") {
+                        ths.addClass(inEffect).css("opacity", "1");
+                    }
+                }, {
+                    offset: "90%"
+                });
+
+            });
+        };
+    })(jQuery);
+
+
+
+
+    //deleted animation for mobile
+    var isMobile = {
+        Android:        function() { return navigator.userAgent.match(/Android/i) ? true : false; },
+        BlackBerry:     function() { return navigator.userAgent.match(/BlackBerry/i) ? true : false; },
+        iOS:            function() { return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false; },
+        Windows:        function() { return navigator.userAgent.match(/IEMobile/i) ? true : false; },
+        any:            function() { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());  }
+    };
+
+    if ( !isMobile.any() ) {
+        $(".Tservices-item").animated("fadeInUp");
+        $("h3").animated("rollIn");
+        $(".services-item").animated("flipInY");
+        $(".works h2").animated("zoomIn");
+        $(".works-left").animated("slideInRight");
+        $(".works-right").animated("slideInLeft");
+        $(".studio-left").animated("slideInLeft");
+        $(".studio-right").animated("slideInRight");
+        $(".works-row li").animated("zoomIn");
+        $(".news-item").animated("fadeInUpBig");
+
+    }
 });
 
